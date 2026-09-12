5f345d758a2aaad2a5211288b8ed7f340b226206
butterfish1918-stack <butterfish1918@googlemail.com>
2026-09-12 21:16:06 +0200

    Upgrade Archive service worker to v10

--- the-archive/sw.js
+++ the-archive/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'archive-v9.0.0';
+const CACHE = 'archive-v10.0.0';
 const CORE = ['./', './index.html', './manifest.webmanifest', './config.js', './icon.svg'];
 
 self.addEventListener('install', event => {
@@ -11,6 +11,13 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin === self.location.origin && url.pathname.endsWith('/index.html')) {
+    event.respondWith(fetch(event.request).then(response => {
+      const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response;
+    }).catch(() => caches.match(event.request)));
+    return;
+  }
   event.respondWith((async () => {
     const cached = await caches.match(event.request);
     const network = fetch(event.request).then(response => {
