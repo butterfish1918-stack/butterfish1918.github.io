e8e9dfde580aa693c4a629c83d5d7979955664c1
butterfish1918-stack <butterfish1918@googlemail.com>
2026-09-12 21:26:37 +0200

    Finalize Archive v10 Android cache

--- the-archive/sw.js
+++ the-archive/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'archive-v10.0.0';
-const CORE = ['./', './index.html', './manifest.webmanifest', './config.js', './icon.svg'];
+const CACHE = 'archive-v10.0.1';
+const CORE = ['./', './index.html', './app.js', './app.css', './storage.js', './manifest.webmanifest', './config.js', './icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
@@ -14,7 +14,9 @@ self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   if (url.origin === self.location.origin && url.pathname.endsWith('/index.html')) {
     event.respondWith(fetch(event.request).then(response => {
-      const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response;
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
     }).catch(() => caches.match(event.request)));
     return;
   }
