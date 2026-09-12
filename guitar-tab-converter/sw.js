1d7c057b3eb92aa35878c9dc95d2a13a03fb8f5c
butterfish1918-stack <butterfish1918@googlemail.com>
2026-09-12 20:37:21 +0200

    Refresh Guitar Workbench PWA cache for v3

--- guitar-tab-converter/sw.js
+++ guitar-tab-converter/sw.js
@@ -1,32 +1,5 @@
-const CACHE = 'gothica-tabs-v3';
-const CORE = [
-  './',
-  './index.html',
-  './app.css',
-  './engine.js',
-  './app.js',
-  './manifest.webmanifest',
-  './icon.svg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
-      return response;
-    }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
-  );
-});
+const CACHE='gothica-tabs-v3';
+const CORE=['./','./index.html','./app.css','./engine.js','./app.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(r&&(r.ok||r.type==='opaque')){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});}return r;}).catch(()=>caches.match(e.request).then(c=>c||(/^https?:/.test(e.request.url)?caches.match('./index.html'):undefined))))});
