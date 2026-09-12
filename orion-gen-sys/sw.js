19cf9ed1eee82a475ef5495f45519a9b6e9eab06
butterfish1918-stack <butterfish1918@googlemail.com>
2026-09-12 20:16:35 +0200

    Deploy Orion GEN-SYS v4 professional mobile UI

--- orion-gen-sys/sw.js
+++ orion-gen-sys/sw.js
@@ -1,12 +1,5 @@
-const CACHE='orion-gen-sys-v3-20260912';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./styles.css','./js/part1.js','./js/part2.js','./js/part3.js','./js/part4.js','./js/part5.js','./js/part6.js','./js/part7.js','./js/part8.js','./js/part9.js'];
+const CACHE='orion-gen-sys-v4-20260912c';
+const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./styles-1.css", "./styles-2.css", "./styles-3.css", "./app-1.html", "./app-2.html", "./app-3.html", "./js/part1.js", "./js/part2.js", "./js/part3.js", "./js/part4.js", "./js/part5.js", "./js/part6.js", "./js/part7.js", "./js/part8.js", "./js/part9.js"];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return response;}).catch(()=>caches.match('./index.html')));
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response;})));
-});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));});
