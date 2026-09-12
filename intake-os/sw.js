127e835699a6bdc06b171d4c8af0a8c9910f76bc
butterfish1918-stack <butterfish1918@googlemail.com>
2026-09-12 19:32:31 +0200

    Scope INTAKE OS service worker cache cleanup

--- intake-os/sw.js
+++ intake-os/sw.js
@@ -1,4 +1,5 @@
-const CACHE = 'intake-os-v3.0.0';
+const CACHE_PREFIX = 'intake-os-';
+const CACHE = `${CACHE_PREFIX}v3.0.1`;
 const CORE = ['./', './index.html', './style.css', './data.js', './audio.js', './visuals.js', './app.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
@@ -7,7 +8,8 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE).map(key => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
