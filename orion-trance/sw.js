b2ab041e337e82f141348fe56262938c9fe583d0
butterfish1918-stack <butterfish1918@googlemail.com>
2026-09-12 19:59:59 +0200

    Add ORION-TRANCE offline service worker

---
+++ orion-trance/sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'orion-trance-v6.4';
+const APP_SHELL = ['./','./index.html','./styles.css','./programmes.js','./ui.js','./engine.js','./render.js','./pwa.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if(event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if(response && response.ok && new URL(event.request.url).origin === self.location.origin) {
+      const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    }
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
